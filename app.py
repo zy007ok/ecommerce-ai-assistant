f6a847954f48cd489b8be22714a3f69f2a36e7c9
zy007ok <zy007ok@gmail.com>
2025-05-22 09:08:12 -0700

    app.py

--- app.py
+++ app.py
@@ -1,5 +1,3 @@
-Python 3.13.3 (v3.13.3:6280bb54784, Apr  8 2025, 10:47:54) [Clang 15.0.0 (clang-1500.3.9.4)] on darwin
-Enter "help" below or click "Help" above for more information.
 >>> import streamlit as st
 ... 
 ... # -----------------------------
